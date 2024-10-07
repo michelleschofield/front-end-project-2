d398aadc8a5456a4ba044a61bfe1683371171128
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-07 20:59:03 +0000

    some styling for menu #1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 'use strict';
+const views = [
+  'home',
+  'study sets',
+  'flashcards',
+  'quiz',
+  'match',
+  'asteroids',
+];
 const $tabHolder = document.querySelector('.tabs');
 if (!$tabHolder) throw new Error('there is no tab holder');
+// function buildMenu(): HTMLDivElement {
+// }
--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,16 @@
+// const views = [
+//   'home',
+//   'study sets',
+//   'flashcards',
+//   'quiz',
+//   'match',
+//   'asteroids',
+// ];
+
 const $tabHolder = document.querySelector('.tabs');
 
 if (!$tabHolder) throw new Error('there is no tab holder');
+
+// function buildMenu(): HTMLDivElement {
+
+// }

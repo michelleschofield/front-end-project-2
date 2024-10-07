9259ca7d17369199d327b10b1b658e88422d1e1c
Michelle Schofield <schofieldshelly05@gmail.com>
2024-10-07 22:08:58 +0000

    menu is created using list of views #1

--- js/main.js
+++ js/main.js
@@ -9,5 +9,22 @@ const views = [
 ];
 const $tabHolder = document.querySelector('.tabs');
 if (!$tabHolder) throw new Error('there is no tab holder');
-// function buildMenu(): HTMLDivElement {
-// }
+buildMenu();
+function buildMenu() {
+  views.forEach((view) => {
+    const $tab = document.createElement('div');
+    const $text = document.createElement('h2');
+    $text.textContent = capitalizeAllWords(view);
+    $tab.className = 'tab';
+    $tab.appendChild($text);
+    $tabHolder?.appendChild($tab);
+  });
+}
+function capitalizeWord(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
+function capitalizeAllWords(words) {
+  const individualWords = words.split(' ');
+  const capitalizedWords = individualWords.map((word) => capitalizeWord(word));
+  return capitalizedWords.join(' ');
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,16 +1,38 @@
-// const views = [
-//   'home',
-//   'study sets',
-//   'flashcards',
-//   'quiz',
-//   'match',
-//   'asteroids',
-// ];
+const views = [
+  'home',
+  'study sets',
+  'flashcards',
+  'quiz',
+  'match',
+  'asteroids',
+];
 
 const $tabHolder = document.querySelector('.tabs');
 
 if (!$tabHolder) throw new Error('there is no tab holder');
 
-// function buildMenu(): HTMLDivElement {
+buildMenu();
 
-// }
+function buildMenu(): void {
+  views.forEach((view) => {
+    const $tab = document.createElement('div');
+    const $text = document.createElement('h2');
+
+    $text.textContent = capitalizeAllWords(view);
+    $tab.className = 'tab';
+
+    $tab.appendChild($text);
+
+    $tabHolder?.appendChild($tab);
+  });
+}
+
+function capitalizeWord(word: string): string {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
+function capitalizeAllWords(words: string): string {
+  const individualWords = words.split(' ');
+  const capitalizedWords = individualWords.map((word) => capitalizeWord(word));
+  return capitalizedWords.join(' ');
+}
